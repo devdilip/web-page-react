497459cbe94912ff737ebf6a0e09b019b60a2c87
Dilip <dilipkr7587@gmail.com>
2019-10-28 00:25:52 +0530

    updtaed style for page content

--- src/Components/PageContent/index.jsx
+++ src/Components/PageContent/index.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import './PageContent.css';
+
 
 export default class PageContent extends Component {
+
+
     render() {
+
         const card = (width, cardDetails) => (
             <div className="card m-1 card-style" style={{ width: `${width}rem` }}>
                 <div className="card-body">
@@ -25,6 +30,39 @@ export default class PageContent extends Component {
                 </div>
             </div>
         );
+
+        const timeCard = () => (
+            <div className="card m-1  card-style time-card-width">
+                <div className="card-body">
+                    <div className="time-cart-title">
+                        <div className="time-card-details">
+                            <h5 className="card-title">
+                                <span style={{ fontSize: "30px" }}>{`07:30:00`}</span>
+                            </h5>
+                            <div className="mt-2 mb-3">27/09/2019</div>
+                            <p className="card-text">
+                                <div>{`STARTING`}</div>
+                                <div>{`TIME`}</div>
+                            </p>
+                        </div>
+                        <div className="border_style1 time-card-details"></div>
+                        <div className="m-3">
+
+                            <h5 className="card-title time-card-time">
+                                <div className='small-time-card'>
+                                    <div className='d-flex justify-content-between time-card-small'>
+                                        <div className='small'>{`TEMPO INIZIALE`}</div>
+                                        <div className='small'>{`07:30:00 27/09/2019`}</div>
+                                    </div>
+                                </div>
+                                <span className='time-count' style={{ fontSize: "40px" }}>{`06:57:24`}</span>
+                            </h5>
+                            <div className="text-center">DURATION</div>
+                        </div>
+                    </div>
+                </div>
+            </div>);
+
         return (
             <div
                 className={`content-container ${
@@ -36,29 +74,7 @@ export default class PageContent extends Component {
                         {card(10, { header: "52", type: "MACHINE", subType: "WORK", progressvalue: 50, isIcon: false, isPercentage: true })}
                         {card(10, { header: "16", type: "ACTIVE", subType: "OPERATORS", progressvalue: 25, isIcon: false, isPercentage: true })}
                         {card(10, { header: "4", type: "PRODUCT", subType: "DISCHARGE", progressvalue: 25, isIcon: true, isPercentage: false })}
-                        <div className="card m-1  card-style" style={{ width: `${26}rem` }}>
-                            <div className="card-body">
-                                <div className="d-flex justify-content-between">
-                                    <div>
-                                        <h5 className="card-title">
-                                            <span style={{ fontSize: "30px" }}>{`07:30:00`}</span>
-                                        </h5>
-                                        <div className="mt-2 mb-3">27/09/2019</div>
-                                        <p className="card-text">
-                                            <div>{`STARTING`}</div>
-                                            <div>{`TIME`}</div>
-                                        </p>
-                                    </div>
-                                    <div className="border_style1"></div>
-                                    <div className="m-3">
-                                        <h5 className="card-title">
-                                            <span style={{ fontSize: "40px" }}>{`06:50:27`}</span>
-                                        </h5>
-                                        <div className="text-center">DURATION</div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        {timeCard()}
                         {card(10, { header: "100", type: "HARDWARE", subType: "PROTYPING", progressvalue: 100, isIcon: false, isPercentage: true })}
                     </div>
 
@@ -70,7 +86,7 @@ export default class PageContent extends Component {
                             <img alt={""} src=""></img>
                         </div>
                     </div>
-                    <div className="card m-1 " style={{ width: `${30}rem` }}>
+                    <div className="card m-1 event-cart ">
                         <div className="card-body">
                             <div className="d-flex ">
                                 <div>
@@ -85,13 +101,13 @@ export default class PageContent extends Component {
                                 </div>
                                 <div className="border_style1"></div>
                                 <div className="m-3">
-                                    <h5 className="card-title">
-                                        <span style={{ fontSize: "40px" }}>{`06:50:27`}</span>
-                                    </h5>
-                                    <div className="text-center">DURATION</div>
+                                    Hello
                                 </div>
                             </div>
                         </div>
+                    </div>
+                    <div className='small-time-card'>
+                        {timeCard()}
                     </div>
                 </div>
             </div>
